05c350ec021d98df34acf4bac7abe7c8bf060f8e
Denys Makhnytskyi <makhnytskyi.denys@gmail.com>
2023-12-02 12:43:27 +0200

    .js&.css commit

---
+++ geniusSpaceCourseJS/js/script.js
@@ -0,0 +1,837 @@
+//***1***
+// Робота зі змінними
+// Оголосіть дві змінні: name та city.
+// Присвойте значення "Іван" змінній name.
+// Скопіюйте значення зі змінної name в city.
+// Виведіть значення змінної city, використовуючи функцію console.log (яка повинна показати “Іван”).
+
+// let name = "Іван";
+// let city = name;
+// console.log(city)
+
+// ***2***
+// Який буде результат виконання скрипта?
+
+// let name = "Olga";
+// console.log(`привіт ${1}`); //
+// console.log(`привіт ${"name"}`); //
+// console.log(`привіт ${name}`); // ?
+
+//***3***
+//Видобути число зі змінних
+// let a = "5";
+// let b = "13cvb";
+// let c = "12.9sxdcfgv";
+// вивести в консоль тип
+// console.log(typeof a);
+// console.log(typeof parseInt(b));
+// console.log(typeof parseFloat(c));
+
+//***4***
+//Зробіть, щоб 0.1 + 0.2 = 0.3
+
+// console.log((0.1*10 + 0.2*10)/10);
+
+//***5**
+//Поверніть найбільше число с набору 20, 10, 50, 40
+
+// console.log(Math.max(20,10,50,40));
+
+//***6**
+//Поверніть випадкове число в діапазоні від 2 до 4
+
+// console.log(Math.random()*(4-2)+2);
+
+//***7**
+//дізнатись довжину message
+const message = 'Welcome to Bahamas!';
+// console.log(message.length)
+
+//***8**
+//вивести в консоль message великими літерами
+// console.log(message.toUpperCase());
+
+//***9**
+// створити пустий об*єкт
+// додати туди ім*я, вік і місто
+// вивести результат в консоль
+// видалити місто
+// додати буль з ключем: like flowers
+// вивести результат в консоль
+
+// let obj = {};
+// obj.name = 'Denys';
+// obj.age = 24;
+// obj.city = 'Khmelnytskyi';
+// delete obj.city;
+// obj['like flowers'] = true;
+
+// console.log(obj);
+
+//***10**
+// За допомогою циклу “for…in” вивести в консоль ключі і значення об*єкта
+
+// for (key in obj){
+//     console.log(key);
+//     console.log(obj[key]);
+// }
+
+// let value;
+
+// String
+// value = false;
+// console.log(typeof value);
+// const newValue = String(value);
+// console.log(typeof newValue);
+// console.log(newValue);
+
+// Number
+// value = '123';
+// console.log(typeof value);
+// const newValue = Number(value);
+// console.log(newValue);
+// console.log(typeof newValue);
+
+// Boolean
+// console.log(Boolean(1))
+// console.log(Boolean(0))
+
+// console.log(Boolean('stroka'))
+// console.log(Boolean(''))
+
+// console.log(Boolean(NaN));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+
+// Ternary operator
+
+// 2+2 === 5 ? console.log('good') : console.log('not good');
+
+// let leng = "hu";
+// if (leng === "ua") {
+//     console.log('ponedilok')
+// } else if (leng === "en"){
+//     console.log('monday');
+// } else {
+//     console.log('not found');
+// }
+
+// let num = prompt("enter value");
+
+// switch (num) {
+//     case 0 :console.log(0);
+//     break;
+//     case 1 : console.log (1);
+//     break;
+//     case 2 : console.log (2);
+//     break;
+//     case 5 : num = 10;
+//     break;
+//     case 10 : console.log (10);
+//     break;
+//     case 9 : console.log (9);
+//     break;
+// }
+
+// let cost;
+// const sub = "premium";
+
+// switch(sub){
+//     case "free":
+//         cost = 0;
+//         break;
+//     case "pro":
+//         cost = 100;
+//         break;
+//     case "premium":
+//         cost = 500;
+// }
+
+// const min = prompt('enter minutes');
+
+// if (min >=0 && min <= 14) {
+//     console.log('1st quarter');
+// } else if(min>=15 && min<= 29){
+//     console.log('2nd quarter');
+// } else if(min>=30 && min<=44){
+//     console.log('3rd quarter');
+// } else if(min>=45 && min<=59){
+//     (console.log('4th quarter'));
+// } else (console.log('There is not such a minute'));
+
+// const value = prompt('enter good name').toLowerCase();
+// let cost;
+
+// switch(value){
+//     case "yabluko" : cost = 15;
+//     console.log(`${value} costs ${cost} UAH`);
+//     break;
+//     case "grusha" : cost = 25;
+//     console.log(`${value} costs ${cost} UAH`);
+//     break;
+//     case "vishnya" : cost = 45;
+//     console.log(`${value} costs ${cost} UAH`);
+//     break;
+//     default : alert(`no goods with name ${value}`);
+// }
+
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах 1, 0, -3.
+
+// let cost = -3;
+// if (cost>0){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test',"qwerty", true
+
+// let value = true;
+// if(value ==="test"){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13.
+
+// let value = 13;
+// if(value > 10){
+//     value -=5;
+//     console.log(value);
+// } else {
+//     value +=5;
+//     console.log(value);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// let value = prompt('enter a value from 1 to 12');
+// let month;
+// switch(value){
+//     case "1" : month = 'january';
+//     console.log(month);
+//     break;
+//     case "2" : month = 'february';
+//     console.log(month);
+//     break;
+//     case '3' : month = 'march';
+//     console.log(month);
+//     break;
+//     case '4' : month = 'april';
+//     console.log(month);
+//     break;
+//     case '5' : month = 'may';
+//     console.log(month);
+//     break;
+//     case '6' : month = 'june';
+//     console.log(month);
+//     break;
+//     case "7" : month = 'july';
+//     console.log(month);
+//     break;
+//     case '8' : month = 'august';
+//     console.log(month);
+//     break;
+//     case '9' : month = 'september';
+//     console.log(month);
+//     break;
+//     case "10" : month = 'october';
+//     console.log(month);
+//     break;
+//     case "11" : month = 'november';
+//     console.log(month);
+//     break;
+//     case "12" : month = 'december';
+//     console.log(month);
+//     break;
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// let value = prompt('enter 3valued number');
+// let result;
+
+// if(value.length === 3){
+//     result = Number(value[0]) + Number(value[1]) + Number(value[2]);
+
+// } else {
+//     console.log('not correct')
+// }
+
+// console.log(result)
+
+// Array
+
+// let arr = ['Apple', 'Orange', 'Banana'];
+
+// console.log(arr[0]);
+
+// arr[2] = 'Lemon';
+// console.log(arr[2]);
+
+// arr[3] = 'Banana';
+// console.log(arr.length);
+
+// arr2 = [1, 'hello', {key: ''}]
+
+// arr.push('Tomato')
+// console.log(arr);
+
+// arr.pop();
+
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// arr.unshift("Cherry");
+// console.log(arr);
+
+// for (let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// for (let item of arr){
+//     console.log(item);
+// }
+
+// for (let item in arr){
+//     console.log(arr[item]);
+// }
+
+// let matrix = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ];
+// console.log(matrix[1][1])  // 5
+
+// Arr methods
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.splice(1,2 );
+// console.log(arr)  // 1,4,5;
+
+// console.log(arr.slice(1,3));
+
+// let a = [6,7,8];
+
+// console.log(arr.concat(a))
+
+// console.log(arr.indexOf(2)); //1
+
+// console.log(arr.includes(2))  // true
+
+// const fruits = [
+// 	{ id: 0, name: 'Apple' },
+// 	{ id: 1, name: 'Tomato' },
+// 	{ id: 2, name: 'Cherry' },
+// 	{ id: 3, name: 'Orange' },
+// ];
+
+// console.log(fruits.find((item) => item.id === 2));
+
+// console.log(fruits.filter((item) => item.name === 'Tomato'));
+
+// MAP
+
+// let result = fruits.map((item) => item.name.length);
+// console.log(result);
+
+// SPLIT
+
+// let names = 'Оля, Юля, Аня, Петя';
+
+// let arr = names.split(', ');
+
+// let newString = arr.join(', ');
+
+// console.log(newString);
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let red = arr.reduce((sum, item) => sum + item);
+
+// console.log(red);
+
+// let arr = [23, 45, 30];
+// let summ = arr.reduce((sum, item) => sum + item) / arr.length;
+
+// console.log(summ);
+
+// -- 1 --
+// У вас є масив об’єктів fruts, і в кожному з них є name
+// Напишіть код, який перетворює їх в масив імен.
+// const fruts = [
+// 	{ id: 0, name: 'Apple' },
+// 	{ id: 1, name: 'Tomat' },
+// 	{ id: 2, name: 'Cherry' },
+// 	{ id: 3, name: 'Orange' },
+// ];
+
+// let names = [];
+// for (item of fruts) {
+// 	names.push(item.name);
+// }
+// console.log(names);
+
+// -- 2 --
+//Виведіть парні числа від 2 до 10, використовуючи цикл for.
+
+// for (let i = 2; i <= 10; i++) {
+// 	if (i % 2 === 1) continue;
+// 	console.log(i);
+// }
+
+// -- 3 --
+//Замініть цикл "for" на "while"
+// for (let i = 0; i < 5; i++) {
+// 	console.log(`цифра ${i}!`);
+// }
+
+// let i = 0;
+// while (i < 5) {
+// 	console.log(`цифра ${i}!`);
+// 	i++;
+// }
+
+// -- 4 --
+//Напишіть цикл, який пропонує prompt ввести число більше за 100.
+//Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
+//Цикл повинен запитувати число доти, доки відвідувач не введе число,
+// більше за 100, або не скасує ввід/введе порожній рядок.
+// let i = 0;
+// do {
+// 	let a = prompt('Enter a number >100');
+// 	i = a;
+// 	console.log(i);
+// } while (i < 100);
+
+// -- 5 --
+// Вирахуйте середній вік
+// const girls = [
+// 	{ age: 23, name: 'Оля' },
+// 	{ age: 29, name: 'Аня' },
+// 	{ age: 10, name: 'Юля' },
+// 	{ age: 20, name: 'Катя' },
+// ];
+
+// let ageCount = [];
+// for (item of girls) {
+// 	ageCount.push(item.age);
+// }
+// console.log(ageCount);
+
+// let red = ageCount.reduce((sum, item) => sum + item) / ageCount.length;
+
+// console.log(red);
+
+// Function expression
+
+// const userName = 'Ann';
+
+// const sayHi = function () {
+// 	let message = 'Hello, my name ';
+// 	console.log(message + userName);
+// };
+
+// sayHi();
+
+// Function declaration
+
+// function sum(a, b) {
+// 	let c = a + b;
+// 	return c;
+// }
+
+// let result = sum(2, 3);
+// console.log(result);
+
+// const testFunc = function (a, b) {
+// 	console.log('a:', a);
+// 	console.log('b:', b);
+// 	return;
+// };
+
+// testFunc(5, 6);
+
+// arguments and rest
+
+// const addName = function () {
+// 	const arg = Array.from(arguments);
+// 	console.log(arguments);
+// 	console.log(arg);
+// };
+
+// addName(1, 3, 4, 5);
+
+// rest = ...
+// const addName = function (...arg) {
+// 	console.log(arg);
+// };
+
+// addName(1, 3, 5);
+
+//callback
+
+// function ask(question, yes, no) {
+// 	if (confirm(question)) {
+// 		yes();
+// 	} else {
+// 		no();
+// 	}
+// }
+
+// function showOk() {
+// 	console.log('You say ok');
+// }
+
+// function showCancel() {
+// 	console.log('You say No');
+// }
+
+// ask('Yes or no?', showOk, showCancel);
+
+//high order function
+
+// const sayHello = function (name) {
+// 	console.log(`Hello ${name}`);
+// };
+
+// const searchName = function (callback) {
+// 	const name = 'Denys';
+// 	callback(name);
+// };
+
+// searchName(sayHello);
+
+// function checkAge(age) {
+// 	if (age > 18) {
+// 		return console.log('Hello');
+// 	}
+// 	return console.log('Good Bye');
+// }
+// checkAge(21);
+
+// function showMovie(age) {
+// 	if (age < 21) {
+// 		return;
+// 	}
+// 	return console.log('Go to movie!');
+// }
+
+// showMovie(21);
+
+// Arrow Function
+// const showMovie = (age) => {
+// 	if (age > 21) {
+// 		return console.log('hello');
+// 	}
+// 	return console.log('Bye!');
+// };
+
+// showMovie(20);
+
+// const a = () => {
+// 	console.log('a');
+// };
+
+// const b = () => {
+// 	console.log('b');
+// };
+
+// const c = () => {
+// 	console.log('c');
+// 	a();
+// 	b();
+// };
+
+// c();
+
+// const addNumber = (number) => {
+// 	let suum = Number(number) + 10;
+// 	return console.log(suum);
+// };
+
+// const numberRef = document.querySelector('input[name="number"]');
+
+// const buttonRef = document.querySelector('button');
+
+// buttonRef.addEventListener('click', () => addNumber(numberRef.value));
+
+// ---1---
+// Перепишіть функцію, використовуючи '?' або '||'
+// function checkAge(age) {
+// 	age > 18 ? true : confirm('Батьки дозволили?');
+// }
+
+// checkAge(19);
+
+// ---2---
+// Напишіть функцію min(a, b), яка повертає менше з двох чисел a та b.
+
+// const minOfTwo = (a, b) => {
+// 	if (a > b) {
+// 		return b;
+// 	}
+// 	return a;
+// };
+
+// console.log(minOfTwo(666, 999));
+
+// ---3---
+// Перепишіть з використанням стрілкових функцій
+// Замініть Функціональні Вирази на стрілкові функції у коді нижче:
+// const ask = (question, yes, no) => {
+// 	if (confirm(question)) yes();
+// 	else no();
+// };
+// ask(
+// 	'Ви згодні?',
+// 	function () {
+// 		alert('Ви погодились.');
+// 	},
+// 	function () {
+// 		alert('Ви скасували виконання.');
+// 	},
+// );
+
+//L25 - Замикання, this
+
+// function createNewSum(num) {
+// 	return function () {
+// 		console.log(10 * num);
+// 	};
+// }
+
+// const calc = createNewSum(5);
+
+// calc();
+
+// function createNewNumber(n) {
+// 	return function (num) {
+// 		return n + num;
+// 	};
+// }
+
+// const addFive = createNewNumber(5);
+
+// console.log(addFive(10));
+
+// function createUrl(domain) {
+// 	return function (url) {
+// 		return `https://${url}.${domain}`;
+// 	};
+// }
+
+// const comUrl = createUrl('com');
+// console.log(comUrl('google'));
+
+// _______________________________ //
+// _______________________________ //
+//this
+
+// function hello() {
+// 	console.log('hello', this);
+// }
+
+// hello();
+
+// const user = {
+// 	name: 'Ivan',
+// 	city: 'Odessa',
+// 	sayHello: hello,
+// };
+
+// user.sayHello();
+
+// function abc() {
+// 	console.log('В функції');
+// 	console.log(this);
+// }
+
+// abc();
+
+// document.querySelector('p').onclick = abc;
+
+// function changeColor() {
+// 	this.style.background = 'green';
+// 	this.style.color = 'white';
+// }
+
+// document.querySelector('div').onclick = changeColor;
+
+// let user = document.querySelectorAll('div');
+
+// user.forEach(function (element) {
+// 	element.onclick = changeColor;
+// });
+
+// const showList = () => {
+// 	console.log(this);
+// };
+
+// showList();
+
+// const list = {
+// 	names: ['Ann', 'Olga', 'Ivan'],
+// 	showList: showList,
+// };
+
+// list.showList();
+
+// function hello() {
+// 	console.log(this);
+// }
+
+// const user = {
+// 	name: 'Ivan',
+// 	age: 30,
+// 	hello: hello,
+// 	sayHelloWindow: hello.bind(window),
+// 	info: function (city) {
+// 		console.log(`Name is ${this.name}`);
+// 		console.log(`Age is ${this.age}`);
+// 		console.log(`City is ${city}`);
+// 	},
+// };
+
+// user.info();
+
+// const Ann = {
+// 	name: 'Anna',
+// 	age: 23,
+// };
+
+// const Nata = {
+// 	name: 'Nata',
+// 	age: 18,
+// };
+
+// user.info.bind(Ann, 'Kyiv')();
+// user.info.bind(Nata, 'Khmelnytskyi')();
+
+// ================ call
+
+// const userInfo = {
+// 	name: 'name',
+// 	age: 98,
+// 	logInfo: function (job) {
+// 		console.group(`${this.name} info:`);
+// 		console.log(`Name is : ${this.name}`);
+// 		console.log(`Age is : ${this.age}`);
+// 		console.log(`Job is : ${job}`);
+// 		console.groupEnd();
+// 	},
+// };
+
+// const Vano = {
+// 	name: 'Ivan',
+// 	age: 45,
+// };
+
+// userInfo.logInfo.call(Vano, 'santehnik');
+
+// ================ apply
+
+// const showUserInfo = {
+// 	name: name,
+// 	age: 97,
+// 	logInfo: function (job, city) {
+// 		console.group(`${this.name} info:`);
+// 		console.log(`Name is : ${this.name}`);
+// 		console.log(`Age is : ${this.age}`);
+// 		console.log(`Job is : ${job}`);
+// 		console.log(`City is : ${city}`);
+// 		console.groupEnd();
+// 	},
+// };
+
+// const Vano = {
+// 	name: 'Ivan',
+// 	age: 45,
+// };
+
+// showUserInfo.logInfo.apply(Vano, ['santehnik', 'Lviv']);
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+// const message1 = function (name, stars) {
+// 	console.log(`${name}, Welcome to ${this.hotel}, stars ${stars}`);
+// };
+
+// const Bukovel = { hotel: 'Bukovel' };
+// const Turist = { hotel: 'Turist' };
+
+// message1.call(Bukovel, 'Ivan', '5');
+// message1.call(Turist, 'Ivan', '3');
+
+// message1.apply(Bukovel, ['Ivan', '5']);
+// message1.apply(Turist, ['Ivan', '3']);
+
+// message1.bind(Bukovel, 'Ivan', '5')();
+// message1.bind(Turist, 'Ivan', '3')();
+
+// const cart = {
+// 	showItems() {
+// 		console.log('В корзині: ', this.items);
+// 	},
+// };
+
+// const woman = {
+// 	items: ['dress', 'tyfli'],
+// };
+
+// const man = {
+// 	items: ['suit', 'shirt'],
+// };
+
+// const kid = {
+// 	items: ['t-shirt', 'pants'],
+// };
+
+// document
+// 	.querySelector('#wom')
+// 	.addEventListener('click', cart.showItems.bind(woman));
+
+// document
+// 	.querySelector('#man')
+// 	.addEventListener('click', cart.showItems.bind(man));
+
+// document
+// 	.querySelector('#kid')
+// 	.addEventListener('click', cart.showItems.bind(kid));
+
+// const infoCar = {
+// 	name: 'BMW',
+// 	model: 'M7',
+// 	color: 'white',
+// 	showInfo: function () {
+// 		console.log(
+// 			`Car ${this.name}, model ${this.model}, color ${this.color}`,
+// 		);
+// 	},
+// };
+
+// infoCar.showInfo();
+
+// car2 = {
+// 	name: 'OPEL',
+// 	model: 'XXX',
+// 	color: 'red',
+// };
+
+// // infoCar.showInfo.bind(car2)();
+
+// infoCar.showInfo.call(car2);
+
+// infoCar.showInfo.apply(car2);
